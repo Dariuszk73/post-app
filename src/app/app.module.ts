7f27a975a8129c61e4f53b9bdd607719eedd7c5d
Dariusz <dariuszklimaszewski@wp.pl>
2020-11-14 13:14:48 +0100

    postsapp: add working v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { PostItemComponent } from './component/post-item/post-item.component';
+import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
+import {AboutComponent} from './component/about/about.component';
+import {PostsListComponent} from "./component/posts-list/posts-list.component";
+import {HomeComponent} from "./component/home/home.component";
+import {HttpClientModule} from "@angular/common/http";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    PostItemComponent,
+    PostsListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
